d1b7d3061dd3fdaa46eb79cec8ac13434e410fb9
Ayush Gurjar <jiaayush256@gmail.com>
2026-06-02 17:24:16 +0530

    Sum of Array.cpp

---
+++ CYNTEXA/Sum of Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
