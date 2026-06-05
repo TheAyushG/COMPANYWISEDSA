f970b23ccd04c463143309fecd32de1099afef31
Ayush Gurjar <jiaayush256@gmail.com>
2026-06-05 15:02:18 +0530

    3. Longest Substring Without Repeating Characters.cpp

--- CYNTEXA/ROUND_2/STRING/3. Longest Substring Without Repeating Characters.cpp
+++ CYNTEXA/ROUND_2/STRING/3. Longest Substring Without Repeating Characters.cpp
@@ -1,3 +1,44 @@
+---------------------------------------------
+Optimal approach
+Time  : O(n)
+Space : O(1)
+---------------------------------------------
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int n = s.size();
+        
+        vector<int> hash(256, -1);
+
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+        
+        while(r<n){
+            if(hash[s[r] != -1]){
+                if(hash[s[r]] >= l){
+                    l = hash[s[r]] + 1;
+                }
+            }
+
+            int len = r-l+1;
+            maxLen = max(len, maxLen);
+            hash[s[r]] = r;
+            r++;
+        }
+
+        return maxLen;
+    }
+};
+
+
+
+*****************************************************************************************************************************************************************************************************************************
+
+
+
 --------------------------------------------
 Brute Force Solution
 Time  : O(n²)
