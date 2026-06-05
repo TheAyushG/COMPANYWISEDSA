fe0ee361b86bbff94d54c9a37bd32e0424442e9f
Ayush Gurjar <jiaayush256@gmail.com>
2026-06-05 17:21:53 +0530

    189. Rotate Array.cpp

---
+++ CYNTEXA/ROUND_2/ARRAY/189. Rotate Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+
+   void rev(vector<int> &nums, int start, int end){
+        while(start<end){
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+   }
+
+public:
+    void rotate(vector<int>& nums, int k) {
+
+    int n = nums.size();
+
+    k = k % n;
+
+    rev(nums, 0, n-1);
+    rev(nums, 0, k-1);
+    rev(nums, k, n-1);
+    
+    }
+};
