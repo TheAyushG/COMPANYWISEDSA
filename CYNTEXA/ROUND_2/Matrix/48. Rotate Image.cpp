ec149a581c3b419bb308111083a9a14c9c641567
Ayush Gurjar <jiaayush256@gmail.com>
2026-06-06 04:27:38 +0530

    48. Rotate Image.cpp

---
+++ CYNTEXA/ROUND_2/Matrix/48. Rotate Image.cpp
@@ -0,0 +1,31 @@
+----------------------------------------------
+Time  : O(n²)
+Space : O(1)
+----------------------------------------------
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<m; j++){
+               swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            int l = 0;
+            int r = n-1;
+            while(l<r){
+                int temp = matrix[i][r];
+                matrix[i][r] = matrix[i][l];
+                matrix[i][l] = temp;
+                l++;
+                r--;
+            }
+        }
+    }
+};
